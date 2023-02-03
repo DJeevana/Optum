616ccdb8ace981c58b77b8437ace3ca67cb59136
DJeevana <124182068+DJeevana@users.noreply.github.com>
2023-02-03 12:29:38 +0530

    Create Currency.cs

---
+++ Currency.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+//Assuming the program would the coin by weight/size. Based on the coin its equivalent dollar value is extracted.
+public static Class Currency
+{
+  Dictionary<string, decimal> exchange=new Dictionary<string,decimal>();
+  exchange.Add("Nickel",0.05);
+  exchange.Add("Dime",0.1);
+  exchange.Add("Quarter",0.25)
+}
