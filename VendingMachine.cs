bede93d5f61c71e2e9d19f62f760e4bba623d4fd
DJeevana <124182068+DJeevana@users.noreply.github.com>
2023-02-03 11:24:48 +0530

    Update VendingMachine.cs

--- VendingMachine.cs
+++ VendingMachine.cs
@@ -6,26 +6,36 @@ using System.Text;
 public Class VendingMachine
 {
       string path = Path.Combine(Environment.CurrentDirectory, "ProductList.txt");
+      List<Product> productList = new List<Product>();
+            
       public VendingMachine()
       {
             InitializeVendingMachine();
+	    DisplayProductList();
       }
       
       public InitializeVendingMachine()
       {
-            List<Product> productList = new List<Product>();
             using (StreamReader sr = new StreamReader(path))
             {
                               while(!sr.EndOfStream)
-					{
-						string currentLine = sr.ReadLine();
-						string[] currentProductAttributes currentLine.Split('$');
+			      {
+				    string currentLine = sr.ReadLine();
+				    string[] currentProductAttributes currentLine.Split('$');
 
-						Product product = new Product(currentProductAttribues);
+				    Product product = new Product(currentProductAttribues);
                                     productList.Add(product);						
                               }
             }
       }
+      public DisplayProductList()
+      {
+      	foreach(Product p in productList)
+	{
+		Console.WriteLine(p.Index()+" Name: "+p.Name+" Price: "+p.Price);
+	}
+      }
+      
       
       
 }
