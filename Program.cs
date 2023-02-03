e8bf5f5e44ef6012dc0427464047bcfcfe6302d7
DJeevana <124182068+DJeevana@users.noreply.github.com>
2023-02-03 11:51:33 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,21 +7,44 @@ Public Class Program
 {
 		public statis void Main(strings[] args)
 		{
+			Product selectedProduct;
+			bool validInput=false;
+			int index;
 			VendingMachine vmInstance=new VendingMachine();
+			
 			Console.Clear();
 			Console.WriteLine("Please select a product"); // Assuming the customer would select the product code which is basically the index starting from 0
-			Console.WriteLine();
+			Console.WriteLine();	
 			
-			int index= int.Parse(Console.ReadLine());
-			try
-			{
-				Product selectedProduct=vmInstance.productList.Get(index);
-			}
-			catch(Eception ex)
-			{
-				Console.WriteLine("Invalid input selected. Please select a valid input.");
-				vmInstance.
-			}
+			do
+			(
+				index= int.Parse(Console.ReadLine());
+				try
+				{
+					selectedProduct=vmInstance.productList.Get(index);
+					if(selectedProduct.Quantity==0)
+					{
+						Console.WriteLine("The selected product is not available in the Inventory. Please select another product. ");
+						vmInstance. DisplayProductList();
+						Console.WriteLine("Please select a product");
+					}
+					else
+					{
+						validInput=true;
+					}
+				}
+				catch(Exception ex)
+				{
+					Console.WriteLine("Invalid input selected. Please select a valid input.");
+					vmInstance. DisplayProductList();
+					Console.WriteLine("Please select a product");
+				}
+			)while(!validInput);
+			
+			Console.WriteLine("Selected product is: "+selectedProduct.Name);
+			Consle.WriteLine("Please proceed for the payment of $: "+selectedProduct.Price);
+			Console.WriteLine("Accepted coins are nickels, dimes and quarters")
+			
 			
 			
 		
