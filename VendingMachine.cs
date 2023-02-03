72a154086e5868a26e25407d034673507600a8aa
DJeevana <124182068+DJeevana@users.noreply.github.com>
2023-02-03 11:11:03 +0530

    Update VendingMachine.cs

--- VendingMachine.cs
+++ VendingMachine.cs
@@ -5,8 +5,27 @@ using System.Text;
 
 public Class VendingMachine
 {
+      string path = Path.Combine(Environment.CurrentDirectory, "ProductList.txt");
       public VendingMachine()
       {
+            InitializeVendingMachine();
       }
+      
+      public InitializeVendingMachine()
+      {
+            List<Product> productList = new List<Product>();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                              while(!sr.EndOfStream)
+					{
+						string currentLine = sr.ReadLine();
+						string[] currentProductAttributes currentLine.Split('$');
+
+						Product product = new Product(currentProductAttribues);
+                                    productList.Add(product);						
+                              }
+            }
+      }
+      
       
 }
